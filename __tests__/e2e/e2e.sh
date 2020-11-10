54058adfb5f7717e76884b33050d262ac56e3c7e
Saul Maldonado <samaldonado4@gmail.com>
2020-11-10 03:51:14 -0600

    tests: add scripts for custom e2e test checks
    extract scripts from .sh files for security

--- __tests__/e2e/e2e.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curl \
-  -X POST \
-  -H "Accept: application/vnd.github.v3+json" \
-  -H "Authorization: token $1" \
-  https://api.github.com/repos/saulmaldonado/skip-workflow/actions/workflows/3497271/dispatches \
-  -d '{"ref":"main"}'
-
